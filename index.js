9a472b1b238e0df26ae4b8c5a9c12e1b1e28af9b
brendan544 <brendan.gwer@student.moringaschool.com>
2024-06-25 18:09:23 +0300

    message

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Alice"));
+
+function introductionWithLanguage(name, language="Javascript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage("Alice", "Javascript"));
+
+
